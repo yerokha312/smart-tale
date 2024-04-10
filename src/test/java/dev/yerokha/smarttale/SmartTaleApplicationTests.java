c5d5e38b06519c45d46af5166a58093b547f660e
yerbolat <erbolatt@live.com>
2024-04-10 13:35:00 +0500

    Initial commit

---
+++ src/test/java/dev/yerokha/smarttale/SmartTaleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yerokha.smarttale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
