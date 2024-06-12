f98db19945cd954ff384e2ede251a95e1faed3ab
yerbolat <erbolatt@live.com>
2024-06-12 10:53:42 +0500

    T1-160 fixed bugs in get all personal ads

--- src/main/java/dev/yerokha/smarttale/dto/AdvertisementDto.java
+++ src/main/java/dev/yerokha/smarttale/dto/AdvertisementDto.java
@@ -1,22 +1,21 @@
 package dev.yerokha.smarttale.dto;
 
 import dev.yerokha.smarttale.enums.PersonalAdvertisementType;
-import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 // common DTO for Order and Product for retrieving both from repo
 public record AdvertisementDto(
-        @NotNull PersonalAdvertisementType type,
-        @NotNull Long advertisementId,
-        @NotNull String title,
-        @NotNull String description,
-        @NotNull BigDecimal price,
-        @NotNull int quantity,
-        @NotNull String imageUrl,
-        @NotNull LocalDateTime publishedAt,
-        @NotNull int acceptancesCount,
-        @NotNull boolean isClosed
+        PersonalAdvertisementType type,
+        Long advertisementId,
+        String title,
+        String description,
+        BigDecimal price,
+        int quantity,
+        String imageUrl,
+        LocalDateTime publishedAt,
+        int acceptancesCount,
+        boolean isClosed
 ) implements AdvertisementInterface {
 }
